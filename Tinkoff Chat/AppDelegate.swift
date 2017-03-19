d37e95cbb2b8f9f3179571624297e37da6a586f5
ADAMBAT1912 <adambat@yandex.ru>
2017-03-19 14:33:27 +0300

    - added life cycle of viewcontroller

--- Tinkoff Chat/AppDelegate.swift
+++ Tinkoff Chat/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
+        print("\(#function)")
         print(UIViewController())
         print(UIButton())
         print(UITextField())
@@ -32,6 +33,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
         
+        print("\(#function)")
         print(UIViewController())
         print(UIButton())
         print(UITextField())
@@ -44,17 +46,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
         
+        print("\(#function)")
         print(UIViewController())
         print(UIButton())
         print(UITextField())
         print(UITextView())
         print(UIImageView())
         print(UIAlertController())
-        print(UIImagePickerController())    }
+        print(UIImagePickerController())   }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
         
+        print("\(#function)")
         print(UIViewController())
         print(UIButton())
         print(UITextField())
@@ -67,6 +71,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
        // print(UIButton())
+        print("\(#function)")
         print(UIViewController())
         print(UIButton())
         print(UITextField())
@@ -79,8 +84,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         //print(UIButton())
+        print("\(#function)")
         print(UIViewController())
-         print(UIButton())
+        print(UIButton())
         print(UITextField())
         print(UITextView())
         print(UIImageView())
